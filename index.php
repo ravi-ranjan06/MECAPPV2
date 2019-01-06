ed3cfa13c5973eee37ecef986cd89a2debe36e3d
Ravi <raviranjan621@gmail.com>
2019-01-06 20:42:37 +0530

    adding index and login pages

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MobilityeCommerce</title>
+</head>
+<body>
+	<?php
+	header('Location:login');
+	?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>MobilityeCommerce</title>
+
+	<!-- vendor css -->
+	<link href="assets/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+	<link href="assets/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+	<link href="assets/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+
+	<!-- MEC CSS -->
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+	<div class="am-signin-wrapper">
+		<div class="am-signin-box">
+			<div class="row no-gutters">
+				<div class="col-lg-5">
+					<div>
+						<h2>MobilityeCommerce</h2>
+						<p>MEC internal tool for checks</p>
+						<hr>
+						<p>Don't have an account? <br> <a href="page-signup.html">Sign up Now</a></p>
+					</div>
+				</div>
+				<div class="col-lg-7">
+					<h5 class="tx-gray-800 mg-b-25">Signin to Your Account</h5>
+					<div class="form-group">
+						<label class="form-control-label">Email:</label>
+						<input type="email" id="email" name="email" class="form-control" placeholder="Enter your email address">
+					</div><!-- form-group -->
+
+					<div class="form-group">
+						<label class="form-control-label">Password:</label>
+						<input type="password" id="password" name="password" class="form-control" placeholder="Enter your password">
+					</div><!-- form-group -->
+
+					<div class="form-group mg-b-20"><a href="">Reset password</a></div>
+					<button type="submit" class="btn btn-block">Sign In</button>
+				</div><!-- col-7 -->
+			</div><!-- row -->
+			<p class="tx-center tx-white-5 tx-12 mg-t-15">Copyright &copy; <?php echo date("Y"); ?>-<?php echo date('Y', strtotime('+1 year'));?>. All Rights Reserved. <a href="http://mobilityecommerce.com/" target="_blank">MobilityeCommerce</a></p>
+		</div><!-- signin-box -->
+	</div><!-- am-signin-wrapper -->
+
+	<script src="assets/lib/jquery/jquery.js"></script>
+	<script src="assets/lib/popper.js/popper.js"></script>
+	<script src="assets/lib/bootstrap/bootstrap.js"></script>
+	<script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+	<script src="assets/js/script.js"></script>
+</body>
+</html>
